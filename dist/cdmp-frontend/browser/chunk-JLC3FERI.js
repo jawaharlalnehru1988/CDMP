1a94173d37817ac940a706ba512ece7522485c01
jawaharlalnehru1988 <nehru00123@gmail.com>
2025-03-27 17:44:28 +0530

    dist

---
+++ dist/cdmp-frontend/browser/chunk-JLC3FERI.js
@@ -0,0 +1,1 @@
+import{Ia as t,Wa as a,Xa as n,kb as s}from"./chunk-BMHFWK6R.js";var p=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=t({type:o,selectors:[["app-dashboard"]],decls:2,vars:0,template:function(e,d){e&1&&(a(0,"p"),s(1,"dashboard works!"),n())},encapsulation:2})};export{p as DashboardComponent};
